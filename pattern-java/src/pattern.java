ce65e7eff96942a76ea230fd0878d48b65ecd3d4
unknown <afi.firda@gmail.com>
2021-07-07 17:05:51 +0700

    add pattern

---
+++ pattern-java/src/pattern.java
@@ -0,0 +1,120 @@
+public class pattern {
+    public static void main(String[] args) {
+        pattern6(50);
+    }
+
+    /*
+        * * * * *
+        *       *
+        *       *
+        *       *
+        * * * * *
+    */
+    public static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n){
+                    System.out.print("* ");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+         * * * * *
+         * # # # *
+         * # # # *
+         * # # # *
+         * * * * *
+     */
+    public static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n){
+                    System.out.print("* ");
+                } else{
+                    System.out.print("# ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+            *
+            *
+        * * * * *
+            *
+            *
+    */
+    public static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 3 || j == 3){ // you can use i == n/2 + 1
+                    System.out.print("* ");
+                } else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+        o o * o o
+        o o * o o
+        * * * * *
+        o o * o o
+        o o * o o
+    */
+    public static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 3 || j == 3){ // you can use n / 2 + 1
+                    System.out.print("* ");
+                } else{
+                    System.out.print("o ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+        * o o o o
+        o * o o o
+        o o * o o
+        o o o * o
+        o o o o *
+    */
+
+    public static void pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j){
+                    System.out.print("* ");
+                } else{
+                    System.out.print("o ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void pattern6(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i + j == n +1 || i == j){
+                    System.out.print("* ");
+                } else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
